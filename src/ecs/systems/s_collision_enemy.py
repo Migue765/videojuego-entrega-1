1c77525aefb88dac6b285f0708f4384ad5ee4fd5
Migue756 <ma.gomeza1@uniades.edu.do>
2026-04-19 23:21:55 -0500

    update create animations with frames and exposions

--- src/ecs/systems/s_collision_enemy.py
+++ src/ecs/systems/s_collision_enemy.py
@@ -1,50 +1,66 @@
 import pygame
 import esper
 
-from src.ecs.components import CTransform, CShape, CTagPlayer, CTagBullet, CTagEnemy
+from src.ecs.components import (
+    CTransform, CSurface, CTagPlayer, CTagBullet, CTagEnemy,
+)
+from src.create import create_explosion
 
 
 def system_bullet_boundary(world: esper.World, window_w: int, window_h: int):
     """Removes bullets that have left the screen."""
     to_delete = []
-    for ent, (_, transform, shape) in world.get_components(CTagBullet, CTransform, CShape):
-        if (transform.x + shape.w < 0 or transform.x > window_w or
-                transform.y + shape.h < 0 or transform.y > window_h):
+    for ent, (_, transform, sprite) in world.get_components(CTagBullet, CTransform, CSurface):
+        w, h = sprite.area.width, sprite.area.height
+        if (transform.x + w < 0 or transform.x > window_w or
+                transform.y + h < 0 or transform.y > window_h):
             to_delete.append(ent)
     for ent in to_delete:
         world.delete_entity(ent, immediate=True)
 
 
-def system_bullet_enemy_collision(world: esper.World):
-    """Destroys a bullet and the enemy it hits on contact."""
-    bullets = list(world.get_components(CTagBullet, CTransform, CShape))
-    enemies = list(world.get_components(CTagEnemy, CTransform, CShape))
+def _entity_rect(transform: CTransform, sprite: CSurface) -> pygame.Rect:
+    return pygame.Rect(int(transform.x), int(transform.y),
+                       sprite.area.width, sprite.area.height)
+
+
+def system_bullet_enemy_collision(world: esper.World, explosion_cfg: dict):
+    """Destroys a bullet and the enemy it hits on contact, spawning an explosion."""
+    bullets = list(world.get_components(CTagBullet, CTransform, CSurface))
+    enemies = list(world.get_components(CTagEnemy, CTransform, CSurface))
 
     to_delete = set()
-    for b_ent, (_, b_tf, b_sh) in bullets:
-        b_rect = pygame.Rect(int(b_tf.x), int(b_tf.y), b_sh.w, b_sh.h)
-        for e_ent, (_, e_tf, e_sh) in enemies:
+    explosions = []
+    for b_ent, (_, b_tf, b_sp) in bullets:
+        b_rect = _entity_rect(b_tf, b_sp)
+        for e_ent, (_, e_tf, e_sp) in enemies:
             if e_ent in to_delete:
                 continue
-            e_rect = pygame.Rect(int(e_tf.x), int(e_tf.y), e_sh.w, e_sh.h)
+            e_rect = _entity_rect(e_tf, e_sp)
             if b_rect.colliderect(e_rect):
                 to_delete.add(b_ent)
                 to_delete.add(e_ent)
+                explosions.append((e_rect.centerx, e_rect.centery))
                 break
 
     for ent in to_delete:
         world.delete_entity(ent, immediate=True)
+    for cx, cy in explosions:
+        create_explosion(world, cx, cy, explosion_cfg)
 
 
-def system_player_enemy_collision(world: esper.World):
-    """Destroys the player when it collides with an enemy."""
-    players = list(world.get_components(CTagPlayer, CTransform, CShape))
-    enemies = list(world.get_components(CTagEnemy, CTransform, CShape))
+def system_player_enemy_collision(world: esper.World, explosion_cfg: dict):
+    """Destroys player and enemy on contact, spawning an explosion at the player."""
+    players = list(world.get_components(CTagPlayer, CTransform, CSurface))
+    enemies = list(world.get_components(CTagEnemy, CTransform, CSurface))
 
-    for p_ent, (_, p_tf, p_sh) in players:
-        p_rect = pygame.Rect(int(p_tf.x), int(p_tf.y), p_sh.w, p_sh.h)
-        for _, (_, e_tf, e_sh) in enemies:
-            e_rect = pygame.Rect(int(e_tf.x), int(e_tf.y), e_sh.w, e_sh.h)
+    for p_ent, (_, p_tf, p_sp) in players:
+        p_rect = _entity_rect(p_tf, p_sp)
+        for e_ent, (_, e_tf, e_sp) in enemies:
+            e_rect = _entity_rect(e_tf, e_sp)
             if p_rect.colliderect(e_rect):
+                create_explosion(world, p_rect.centerx, p_rect.centery, explosion_cfg)
+                create_explosion(world, e_rect.centerx, e_rect.centery, explosion_cfg)
                 world.delete_entity(p_ent, immediate=True)
-                break
+                world.delete_entity(e_ent, immediate=True)
+                return
--- src/ecs/systems/s_screen_bounce.py
+++ src/ecs/systems/s_screen_bounce.py
@@ -1,22 +1,27 @@
 import esper
 
-from src.ecs.components import CTransform, CVelocity, CShape, CTagEnemy
+from src.ecs.components import CTransform, CVelocity, CSurface, CTagEnemy, CTagHunter
 
 
 def system_bounce(world: esper.World, window_w: int, window_h: int):
-    """Bounces enemies off screen edges (does not affect player or bullets)."""
-    for _, (transform, velocity, shape, _) in world.get_components(
-            CTransform, CVelocity, CShape, CTagEnemy):
+    """Bounces non-hunter enemies off screen edges."""
+    hunters = {ent for ent, _ in world.get_component(CTagHunter)}
+    for ent, (transform, velocity, sprite, _) in world.get_components(
+            CTransform, CVelocity, CSurface, CTagEnemy):
+        if ent in hunters:
+            continue
+        w = sprite.area.width
+        h = sprite.area.height
         if transform.x < 0:
             transform.x = 0
             velocity.vx = abs(velocity.vx)
-        elif transform.x + shape.w > window_w:
-            transform.x = window_w - shape.w
+        elif transform.x + w > window_w:
+            transform.x = window_w - w
             velocity.vx = -abs(velocity.vx)
 
         if transform.y < 0:
             transform.y = 0
             velocity.vy = abs(velocity.vy)
-        elif transform.y + shape.h > window_h:
-            transform.y = window_h - shape.h
+        elif transform.y + h > window_h:
+            transform.y = window_h - h
             velocity.vy = -abs(velocity.vy)
